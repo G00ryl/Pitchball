78d58affbde2fb44a374ca7e061f130e1e62d6e4
Michał B <MadTiger2409@users.noreply.github.com>
2019-04-09 00:45:48 +0200

    Working on tests

--- Pitchball/Pitchball.Tests/Services/UserService.cs
+++ Pitchball/Pitchball.Tests/Services/UserService.cs
@@ -1,9 +1,14 @@
-﻿using Pitchball.Infrastructure.Data;
+﻿using Moq;
+using Pitchball.Infrastructure.Commands.Account;
+using Pitchball.Infrastructure.Data;
+using Pitchball.Infrastructure.Extensions.Interfaces;
 using Pitchball.Infrastructure.Services;
 using Pitchball.Infrastructure.Services.Interfaces;
 using Pitchball.Tests.Arrange;
+using Pitchball.Tests.Arrange.DataAttributes;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using Xunit;
 
@@ -19,11 +24,26 @@ namespace Pitchball.Tests.Services
             this.fixture = fixture;
         }
 
-        [Fact]
-        public void AddUser_When_CorrectDataProvided()
+        [Theory]
+        [CreateUserValidData]
+        public void AddUser_When_CorrectDataProvided(CreateAccount command, byte[] expectedPasswordHash, byte[] expectedSalt)
         {
-            // need to mock PasswordManager first
-            // IUserService userService = new UserService(fixture.Context)
+            // Arrange
+            var passwordManager = new Mock<IPasswordManager>();
+            passwordManager.Setup(p => p.CalculatePasswordHash(It.IsAny<string>(), out expectedPasswordHash, out expectedSalt));
+            IUserService userService = new UserService(fixture.Context, passwordManager.Object);
+
+            // Act
+            userService.AddAsync(command);
+            var user = fixture.Context.Users.SingleOrDefault(x => x.Id == 1);
+
+            // Assert
+            Assert.Equal(command.Name, user.Name);
+            Assert.Equal(command.Surname, user.Surname);
+            Assert.Equal(command.Login, user.Login);
+            Assert.Equal(command.Email, user.Email);
+            Assert.Equal(expectedPasswordHash, user.PasswordHash);
+            Assert.Equal(expectedSalt, user.Salt);
         }
     }
 }
