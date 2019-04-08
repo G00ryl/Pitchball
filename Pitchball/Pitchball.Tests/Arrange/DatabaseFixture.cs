985b04c95db7f3b4f14409647b6fa865cdeaf3e4
Michał B <cyberwolf_96@outlook.com>
2019-04-08 14:48:51 +0200

    Working on tests project

---
+++ Pitchball/Pitchball.Tests/Arrange/DatabaseFixture.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Pitchball.Infrastructure.Data;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Pitchball.Tests.Arrange
+{
+    public class DatabaseFixture : IDisposable
+    {
+        public PitchContext Context { get; private set; }
+
+        public DatabaseFixture()
+        {
+            var options = new DbContextOptionsBuilder<PitchContext>()
+                .UseInMemoryDatabase(databaseName: "test_database")
+                .Options;
+            Context = new PitchContext(options);
+        }
+
+        public void Dispose()
+        {
+            Context.Dispose();
+        }
+    }
+}
---
+++ Pitchball/Pitchball.Tests/Services/UserService.cs
@@ -0,0 +1,29 @@
+﻿using Pitchball.Infrastructure.Data;
+using Pitchball.Infrastructure.Services;
+using Pitchball.Infrastructure.Services.Interfaces;
+using Pitchball.Tests.Arrange;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Pitchball.Tests.Services
+{
+    [Collection("User Service Tests")]
+    public class UserServiceShould : IClassFixture<DatabaseFixture>
+    {
+        DatabaseFixture fixture;
+
+        public UserServiceShould(DatabaseFixture fixture)
+        {
+            this.fixture = fixture;
+        }
+
+        [Fact]
+        public void AddUser_When_CorrectDataProvided()
+        {
+            // need to mock PasswordManager first
+            // IUserService userService = new UserService(fixture.Context)
+        }
+    }
+}
