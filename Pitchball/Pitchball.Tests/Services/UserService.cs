2a78d2c91abcc19990cd3e2403b0857076a2bbde
Michał B <MadTiger2409@users.noreply.github.com>
2019-04-09 19:06:27 +0200

    Finished tests for user service.

--- Pitchball/Pitchball.Tests/Services/UserService.cs
+++ Pitchball/Pitchball.Tests/Services/UserService.cs
@@ -1,6 +1,8 @@
 ﻿using Moq;
+using Pitchball.Domain.Models;
 using Pitchball.Infrastructure.Commands.Account;
 using Pitchball.Infrastructure.Data;
+using Pitchball.Infrastructure.Extensions.Exceptions;
 using Pitchball.Infrastructure.Extensions.Interfaces;
 using Pitchball.Infrastructure.Services;
 using Pitchball.Infrastructure.Services.Interfaces;
@@ -15,23 +17,23 @@ using Xunit;
 namespace Pitchball.Tests.Services
 {
     [Collection("User Service Tests")]
-    public class UserServiceShould : IClassFixture<DatabaseFixture>
+    public class UserService : IClassFixture<DatabaseFixture>
     {
         DatabaseFixture fixture;
 
-        public UserServiceShould(DatabaseFixture fixture)
+        public UserService(DatabaseFixture fixture)
         {
             this.fixture = fixture;
         }
 
         [Theory]
         [CreateUserValidData]
-        public void AddUser_When_CorrectDataProvided(CreateAccount command, byte[] expectedPasswordHash, byte[] expectedSalt)
+        public void AddAsync_Should_AddUser_When_CorrectDataProvided(CreateAccount command, byte[] expectedPasswordHash, byte[] expectedSalt)
         {
             // Arrange
             var passwordManager = new Mock<IPasswordManager>();
             passwordManager.Setup(p => p.CalculatePasswordHash(It.IsAny<string>(), out expectedPasswordHash, out expectedSalt));
-            IUserService userService = new UserService(fixture.Context, passwordManager.Object);
+            IUserService userService = new Infrastructure.Services.UserService(fixture.Context, passwordManager.Object);
 
             // Act
             userService.AddAsync(command);
@@ -44,6 +46,21 @@ namespace Pitchball.Tests.Services
             Assert.Equal(command.Email, user.Email);
             Assert.Equal(expectedPasswordHash, user.PasswordHash);
             Assert.Equal(expectedSalt, user.Salt);
+        }
+
+        [Theory]
+        [CreateUserValidData]
+        public void AddAsync_Should_ThrowException_When_UserAlreadyExists(CreateAccount command, byte[] expectedPasswordHash, byte[] expectedSalt)
+        {
+            // Arrange
+            var passwordManager = new Mock<IPasswordManager>();
+            passwordManager.Setup(p => p.CalculatePasswordHash(It.IsAny<string>(), out expectedPasswordHash, out expectedSalt));
+            IUserService userService = new Infrastructure.Services.UserService(fixture.Context, passwordManager.Object);
+            fixture.Context.Users.Add(new User(command.Name, command.Surname, command.Login, command.Email, expectedSalt, expectedPasswordHash));
+            fixture.Context.SaveChanges();
+
+            // Act and Assert
+            Assert.ThrowsAsync<CorruptedOperationException>(() => userService.AddAsync(command));
         }
     }
 }
