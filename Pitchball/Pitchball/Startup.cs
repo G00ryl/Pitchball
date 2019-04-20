6845c8a08da39918c48fd5eb0944e99d5c6dfc1a
Michał B <cyberwolf_96@outlook.com>
2019-04-20 20:24:27 +0200

    Fixed all the issues with data seeding.

--- Pitchball/Pitchball/Startup.cs
+++ Pitchball/Pitchball/Startup.cs
@@ -130,7 +130,7 @@ namespace Pitchball
 			app.UseHttpsRedirection();
 			app.UseStaticFiles();
 			app.UseCookiePolicy();
-            //app.UseSession();
+            app.UseSession();
 
             app.UseMvc(routes =>
 			{
