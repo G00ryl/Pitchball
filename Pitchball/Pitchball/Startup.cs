68d2229e6d88eb83f8a88897d12275fd989cb66a
Michał B <MadTiger2409@users.noreply.github.com>
2019-04-30 16:12:00 +0200

    Fixed no cookie issue (hotfix for now)

--- Pitchball/Pitchball/Startup.cs
+++ Pitchball/Pitchball/Startup.cs
@@ -129,7 +129,7 @@ namespace Pitchball
 
 			app.UseHttpsRedirection();
 			app.UseStaticFiles();
-			app.UseCookiePolicy();
+			//app.UseCookiePolicy();
             app.UseSession();
 
             app.UseMvc(routes =>
