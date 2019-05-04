987009430a216f67f8d83c8479708146c52d8c52
Michał B <MadTiger2409@users.noreply.github.com>
2019-05-04 14:30:59 +0200

    Working on

---
+++ Pitchball/Pitchball/Controllers/ReservationController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Pitchball.Controllers
+{
+    public class ReservationController : Controller
+    {
+        public async Task<IActionResult> CreateAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<IActionResult> GetAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<IActionResult> GetAsync()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
