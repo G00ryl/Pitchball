c25f642e4ba8ea6e9e24384d2ee2e1392e3d3e37
Michał B <MadTiger2409@users.noreply.github.com>
2019-05-04 17:36:56 +0200

    Working on

--- Pitchball/Pitchball/Controllers/ReservationController.cs
+++ Pitchball/Pitchball/Controllers/ReservationController.cs
@@ -6,19 +6,35 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace Pitchball.Controllers
 {
+    [Route("reservations")]
     public class ReservationController : Controller
     {
+        [HttpPost("create")]
         public async Task<IActionResult> CreateAsync()
         {
             throw new NotImplementedException();
         }
 
+        [HttpGet("{id}")]
         public async Task<IActionResult> GetAsync(int id)
         {
             throw new NotImplementedException();
         }
 
-        public async Task<IActionResult> GetAsync()
+        [HttpGet("pitch/{id}")]
+        public async Task<IActionResult> GetForPitchAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpGet("captain/{id}")]
+        public async Task<IActionResult> GetForCaptainAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpPost("delete/{id}")]
+        public async Task<IActionResult> DeleteAsync(int id)
         {
             throw new NotImplementedException();
         }
