1665d755d548dfaba1807e6b44f38458e7fc3a0b
Michał B <MadTiger2409@users.noreply.github.com>
2019-05-05 21:57:27 +0200

    Created validator for CreateReservation command.

---
+++ Pitchball/Pitchball/Validators/Reservation/CreateReservationValidator.cs
@@ -0,0 +1,32 @@
+﻿using FluentValidation;
+using Pitchball.Infrastructure.Commands.Reservation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Pitchball.Validators.Reservation
+{
+    public class CreateReservationValidator : AbstractValidator<CreateReservation>
+    {
+        public CreateReservationValidator()
+        {
+            RuleFor(x => x.Name)
+                .NotEmpty()
+                .MinimumLength(5);
+
+            RuleFor(x => x.PitchId)
+                .NotEmpty()
+                .GreaterThan(0);
+
+            RuleFor(x => x.StartDate)
+                .NotEmpty()
+                .GreaterThan(DateTime.UtcNow)
+                .LessThan(x => x.EndDate);
+
+            RuleFor(x => x.EndDate)
+                .NotEmpty()
+                .GreaterThan(x => x.StartDate);
+        }
+    }
+}
