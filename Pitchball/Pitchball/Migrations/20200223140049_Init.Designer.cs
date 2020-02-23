bf1d723c147bcd6bee5a2c253e3a59e612442c7c
Michał B <MadTiger2409@users.noreply.github.com>
2020-02-23 15:05:22 +0100

    First 9 pitches and images. New migration

--- Pitchball/Pitchball/Migrations/20200223140049_Init.Designer.cs
+++ Pitchball/Pitchball/Migrations/20200223140049_Init.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Pitchball.Infrastructure.Data;
 
 namespace Pitchball.Migrations
 {
     [DbContext(typeof(PitchContext))]
-    partial class PitchContextModelSnapshot : ModelSnapshot
+    [Migration("20200223140049_Init")]
+    partial class Init
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
